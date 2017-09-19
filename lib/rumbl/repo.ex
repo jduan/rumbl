913b86f52a5136181a2bc77c37715ef6849801f1
Jingjing Duan <jingjing.duan@airbnb.com>
2017-09-18 22:51:43 -0700

    add User controller

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,11 +1,28 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
-
-  @doc """
-  Dynamically loads the repository url from the
-  DATABASE_URL environment variable.
+  @moduledoc """
+  In memory repository.
   """
-  def init(_, opts) do
-    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+
+  def all(RumblWeb.User) do
+    [
+      %RumblWeb.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
+      %RumblWeb.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
+      %RumblWeb.User{id: "3", name: "Bruce", username: "chrismccord", password: "phx"},
+    ]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    all(module)
+    |> Enum.find(fn map -> map.id == id end)
+  end
+
+  def get_by(module, params) do
+    all(module)
+    |> Enum.find(fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+    )
   end
 end
---
+++ lib/rumbl_web/models/user.ex
@@ -0,0 +1,3 @@
+defmodule RumblWeb.User do
+  defstruct [:id, :name, :username, :password]
+end
