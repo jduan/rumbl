f21fdc0743cd268138e61283b488504c5ee6c0ec
Jingjing Duan <jingjing.duan@airbnb.com>
2017-09-18 23:15:40 -0700

    user Ecto and create User model

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,28 +1,3 @@
 defmodule Rumbl.Repo do
-  @moduledoc """
-  In memory repository.
-  """
-
-  def all(RumblWeb.User) do
-    [
-      %RumblWeb.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
-      %RumblWeb.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
-      %RumblWeb.User{id: "3", name: "Bruce", username: "chrismccord", password: "phx"},
-    ]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    all(module)
-    |> Enum.find(fn map -> map.id == id end)
-  end
-
-  def get_by(module, params) do
-    all(module)
-    |> Enum.find(fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-    )
-  end
+  use Ecto.Repo, otp_app: :rumbl
 end
